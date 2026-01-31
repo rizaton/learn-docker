107470c9c30dbe765110ff7d0fcfe65d0a989480
rizaton <123567427+rizaton@users.noreply.github.com>
2026-02-01 01:59:27 +0700

    command instruction

--- script.sh
+++ script.sh
@@ -1,5 +1,20 @@
+#FROM Instruction
 docker build -t rizaton/from from
 
 docker image ls
 
+#RUN Instruction
+docker build -t rizaton/run run
+
 docker build -t rizaton/run run --progress=plain --no-cache
+
+#CMD Instruction
+docker build -t rizaton/command command
+
+docker image inspect rizaton/command
+
+docker container create --name command rizaton/command
+
+docker container start command
+
+docker container logs command
